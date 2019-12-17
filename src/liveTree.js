db96a56f176f194b1f7aa9ff60bcd3ccb0512e2e
Jackie Zhou <Jacqueline.colleen88@gmail.com>
2019-12-17 14:01:32 -0500

    first heroku commit

---
+++ src/liveTree.js
@@ -0,0 +1,11 @@
+// Frame: Create a live-updating tree view as a web application
+// Must contain groups of nodes w/ main node that can house any # of factories
+// Each factory node can generate a pre-set amount of random numbers (max 15) rep as children of their resp. parents
+// Create though user input (button) specifying the # of children to generate & ranges of said children
+// Each factory must be removable, name editable & have lower/upper bound for the random num generation ---- ????
+
+// Changes should be immediate w/o refreshing (Name of that free server where I can do this, Fire something)
+// State of tree should be persistent & not be undone w/ reloading
+// All of a factories existing child does should be removed up each new generation ---- ????
+// Add security ---- ????
+// Host on Heroku
