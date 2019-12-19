2c58fe71e8b1331e2c29fa224b92c7025c75ad39
Jackie Zhou <Jacqueline.colleen88@gmail.com>
2019-12-19 15:39:39 -0500

    connecting firebase

--- helloworld/app.js
+++ helloworld/app.js
@@ -1,54 +1,44 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyBJbH6n92KJxfGCirXHv_VFM0A1GwwZGU0",
-    authDomain: "live-tree-16eb2.firebaseapp.com",
-    databaseURL: "https://live-tree-16eb2.firebaseio.com",
-    projectId: "live-tree-16eb2",
-    storageBucket: "live-tree-16eb2.appspot.com",
-    messagingSenderId: "1040801579497",
-    appId: "1:1040801579497:web:ea0f283b6ed09d53cbcb78",
-    measurementId: "G-59GL88Y848"
-};
+var database = firebase.database();
+var newEntry = database.ref();
+
+var destEntered = "";
+var nextEntered = "";
+
+$("#submit").on("click", function(event) {
+    event.preventDefault();
+
+    trainEntered = $("#train-input").val().trim();
+    destEntered = $("#destination-input").val().trim();
+    
 
 
-firebase.initializeApp(config);
-
-
-const dbRef = firebase.database().ref();
-
-const usersRef = dbRef.child('users');
-const userListUI = document.getElementById("userList");
-
-usersRef.on("child_added", snap => {
-
-    let user = snap.val();
-
-    let $li = document.createElement("li");
-    $li.innerHTML = user.name;
-    $li.setAttribute("child-key", snap.key);
-    $li.addEventListener("click", userClicked)
-    userListUI.append($li);
-
+    newEntry.push({
+        train: trainEntered,
+        destination: destEntered,
+        
+    })
 });
 
 
-function userClicked(e) {
+newEntry.on("child_added", function(childSnapshot) {
+    var newAdd = childSnapshot.val();
+    
+    console.log("Name: " + newAdd.train);
+    console.log("Number of Fans: " + newAdd.destination);
+    
+    var tBody = $("tbody")
+    var tRow = $("<tr>");
 
-    var userID = e.target.getAttribute("child-key");
-
-    const userRef = dbRef.child('users/' + userID);
-    const userDetailUI = document.getElementById("userDetail");
-
-    userDetailUI.innerHTML = ""
-
-    userRef.on("child_added", snap => {
-
-
-        var $p = document.createElement("p");
-        $p.innerHTML = snap.key + " - " + snap.val()
-        userDetailUI.append($p);
+    var trainTd = $("<td>").text(newAdd.train);
+    var destinationTd = $("<td>").text(newAdd.destination);
 
 
-    });
+    tRow.append(trainTd, destinationTd);
+    tBody.append(tRow);
+    $("#table").append(tBody);
 
-}
 
+    
+    
+
+});
