723569d0c9e623b02888013275cdd8b772ae7bfa
Jackie Zhou <Jacqueline.colleen88@gmail.com>
2019-12-20 12:54:31 -0500

    starting again

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
 var database = firebase.database();
 var newEntry = database.ref();
 
-var destEntered = "";
-var nextEntered = "";
-var freqEntered = "";
-var trainEntered = "";
-var minutesAway = "";
+var fansEntered = "";
+var nameEntered = "";
+
 
 $("#submit").on("click", function(event) {
     event.preventDefault();
 
-    trainEntered = $("#train-input").val().trim();
-    destEntered = $("#destination-input").val().trim();
-    startEntered = $("#start-input").val().trim();
-    freqEntered = $("#freq-input").val().trim();
+    nameEntered = $("#name-input").val().trim();
+    fansEntered = $("#fans-input").val().trim();
     
 
 
     newEntry.push({
-        train: trainEntered,
-        destination: destEntered,
-        startTime: startEntered,
-        frequency: freqEntered,
-        // minutesAway: timeft,
+        Name: nameEntered,
+        Fans: fansEntered,
         
     })
 
@@ -33,43 +26,21 @@ $("#submit").on("click", function(event) {
 newEntry.on("child_added", function(childSnapshot) {
     var newAdd = childSnapshot.val();
     
-    console.log("Train: " + newAdd.train);
-    console.log("Destinaion: " + newAdd.destination);
-    console.log("Start Time: " + newAdd.startTime);
-    console.log("Frequency: " + newAdd.frequency);
+    console.log("Name: " + newAdd.name);
+    console.log("Fans: " + newAdd.fans);
     
     var tBody = $("tbody")
     var tRow = $("<tr>");
 
-    var trainTd = $("<td>").text(newAdd.train);
-    var destinationTd = $("<td>").text(newAdd.destination);
-    var frequencyTd = $("<td>").text(newAdd.frequency);
-
-    
-    var currentTime = moment();
-    var firstTime = newAdd.startTime;
-    var frequency = newAdd.frequency;
-    // difference in time between when 1st train left & current time
-    var diffTime = moment().diff(moment(firstTime, "HH:mm"), "minutes");
-    console.log(diffTime);
-    //remainder = minutes into last train (ie: train is on a 32 min trip, it's 28 mins into trip, there's 4 minutes left, 28 is the num calc)
-    var timePassed = diffTime % frequency;
-    // minutes difference from amount of time train has "traveled" to amount of time before it's arrival
-    var timeLeft = frequency - timePassed;
-    console.log(timeLeft)
-    var nextTrain = moment().add(timeLeft, "minutes");
-    var nextTrainTime = $("<td>").text(moment(nextTrain).format("hh:mm"));
-    console.log(nextTrain);
-    var minAwayTd = $("<td>").text(timeLeft);
+    var trainTd = $("<td>").text(newAdd.name);
+    var destinationTd = $("<td>").text(newAdd.fans);
 
 
 
-    tRow.append(trainTd, destinationTd, frequencyTd, nextTrainTime, minAwayTd);
+
+    tRow.append(nameTd, fansTd);
     tBody.append(tRow);
     $("#table").append(tBody);
-
-
-    
     
 
 });
