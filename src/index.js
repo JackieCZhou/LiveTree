bf7151147a202bc897d403d31dbd5583ec96778c
Jackie Zhou <Jacqueline.colleen88@gmail.com>
2019-12-31 11:53:33 -0500

    relinking styles

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+// var firebaseConfig = {
+//     apiKey: "AIzaSyAG-qx615hR4eDgP1DAmPKhw0Js-o-DFyM",
+//     authDomain: "livetree-65129.firebaseapp.com",
+//     databaseURL: "https://livetree-65129.firebaseio.com/",
+//     projectId: "livetree-65129",
+//     storageBucket: "livetree-65129.appspot.com",
+//     messagingSenderId: "789557141134",
+//     appId: "1:789557141134:web:5278c226c72779633873df"
+// };
+// firebase.initializeApp(firebaseConfig);
+//     // Initialize Firebase
