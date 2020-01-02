c97a2a3e0382f2ba56c8f2bcab5d58cd723d674f
Jackie Zhou <Jacqueline.colleen88@gmail.com>
2020-01-02 16:35:02 -0500

    retrying firebase

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 
-// var firebaseConfig = {
-//     apiKey: "AIzaSyAG-qx615hR4eDgP1DAmPKhw0Js-o-DFyM",
-//     authDomain: "livetree-65129.firebaseapp.com",
-//     databaseURL: "https://livetree-65129.firebaseio.com/",
-//     projectId: "livetree-65129",
-//     storageBucket: "livetree-65129.appspot.com",
-//     messagingSenderId: "789557141134",
-//     appId: "1:789557141134:web:5278c226c72779633873df"
-// };
-// firebase.initializeApp(firebaseConfig);
-//     // Initialize Firebase
+//import firebase from 'firebase';
+
+
+// const app = firebase.initializeApp({
+    //     apiKey: "AIzaSyAG-qx615hR4eDgP1DAmPKhw0Js-o-DFyM",
+    //     authDomain: "livetree-65129.firebaseapp.com",
+    //     databaseURL: "https://livetree-65129.firebaseio.com/",
+    //     projectId: "livetree-65129",
+    //     storageBucket: "livetree-65129.appspot.com",
+    //     messagingSenderId: "789557141134",
+    //     appId: "1:789557141134:web:5278c226c72779633873df"
+// });
+// 
+// const storage = app.storage();
+// const ref = storage.ref('path');
+
