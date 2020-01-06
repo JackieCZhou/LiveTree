a88469cc05a5ab6513c568e89c630da86fdefd2f
Jackie Zhou <Jacqueline.colleen88@gmail.com>
2020-01-06 15:01:38 -0500

    structuring firebse

--- public/app.js
+++ public/app.js
@@ -1,6 +1,21 @@
 document.addEventListener("DOMContentLoaded", event => {
     const app = firebase.app();
-    console.log(app);
+
+    const db = firebase.firestore();
+
+    const myPost = db.collection('women').doc("Jackie");
+    const allInfo = db.collection('men').doc("Chang");
+
+    myPost.onSnapshot(doc => {
+        const data = doc.data();
+        document.write(data.Chang + data.Fans + '<br>')
+    })
+    allInfo.onSnapshot(doc => {
+        const data = doc.data();
+        document.write(data.Fans + '<br>')
+    })
+
+
 });
 
 function googleLogin() {
@@ -10,8 +25,76 @@ function googleLogin() {
 
         .then(result => {
             const user = result.user;
-            document.write('Hello  ${user.displayName}');
+            document.write('Hello ${ user.displayName }');
             console.log(user)
         })
         .catch(console.log)
+}
+
+// <!-- script for list generation under tabs -->
+
+function addLi() {
+    e.preventDefault();
+    var txtVal = document.getElementById("txtVal").value,
+
+        listNode = document.getElementById("list"),
+        liNode = document.createElement("LI"),
+        txtNode = document.createTextNode(txtVal);
+
+    liNode.appendChild(txtNode);
+    listNode.appendChild(liNode);
+
+    console.log("txtVal");  // was working before I began adding random math function, will get back to once that it working.  Won't stop reloading pg
+}
+
+
+// onHover function
+$(document).ready(function () {
+    $('[data-toggle="popover"]').popover({
+        placement: 'top',
+        trigger: 'hover'
+    });
+});
+
+// hide/reveal form onClick
+$(document).ready(function () {
+    $("#formButton").click(function () {
+        $("#form1").toggle();
+    });
+});
+
+$(document).ready(function () {
+    $("#hide").click(function () {
+        $("#form1").toggle();
+    });
+});
+
+
+// build and auto populate list
+
+var numList = [];
+
+function myFunction() {
+    var rnd = Math.floor(Math.random() * 10);
+    document.getElementById('mathVal').value = rnd;
+    numList.push(rnd);
+
+    console.log(numList);
+
+    buildList();
+}
+
+
+function buildList() {
+
+    var newNum = document.getElementsByName("numList"[0]),
+
+        listNode = document.getElementById("newList"),
+        liNode = document.createElement("LI"),
+        txtNode = document.createTextNode(newNum);
+
+    liNode.appendChild(txtNode);
+    listNode.appendChild(liNode);
+
+    console.log(newNum);
 }
