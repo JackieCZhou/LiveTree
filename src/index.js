eb83d7d5eab74c425522aa357d868e2225cfa378
Jackie Zhou <Jacqueline.colleen88@gmail.com>
2020-01-13 11:06:50 -0500

    relinking forebase heroku

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 
-// import firebase from 'firebase';
+import firebase from 'firebase';
 
 
-  // Your web app's Firebase configuration
-//   var firebaseConfig = {
-//     apiKey: "AIzaSyAG-qx615hR4eDgP1DAmPKhw0Js-o-DFyM",
-//     authDomain: "livetree-65129.firebaseapp.com",
-//     databaseURL: "https://livetree-65129.firebaseio.com",
-//     projectId: "livetree-65129",
-//     storageBucket: "livetree-65129.appspot.com",
-//     messagingSenderId: "789557141134",
-//     appId: "1:789557141134:web:5278c226c72779633873df"
-//   };
-//   // Initialize Firebase
-//   firebase.initializeApp(firebaseConfig);
+//   Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAG-qx615hR4eDgP1DAmPKhw0Js-o-DFyM",
+    authDomain: "livetree-65129.firebaseapp.com",
+    databaseURL: "https://livetree-65129.firebaseio.com",
+    projectId: "livetree-65129",
+    storageBucket: "livetree-65129.appspot.com",
+    messagingSenderId: "789557141134",
+    appId: "1:789557141134:web:5278c226c72779633873df"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
