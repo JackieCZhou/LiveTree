b627cda8c1c9948ed20af9de1261e9f042ae24a6
Jackie Zhou <Jacqueline.colleen88@gmail.com>
2020-01-13 10:39:34 -0500

    heroku deploy error, adding back in file

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+
+app.use(express.static(path.join(__dirname, "./public")));
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+
+
+app.listen(PORT, function() {
+  console.log("Listening on PORT: " + PORT);
+});
